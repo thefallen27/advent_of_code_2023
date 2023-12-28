318329541d51303e47943eff4c64bfde2316c79b
the_fallen_27 <mariopgsco@gmail.com>
2023-12-28 23:56:44 +0200

    Adding the implementation(s) for day 8, part 1

---
+++ day_8/day_8_a_1.cpp
@@ -0,0 +1,48 @@
+#include <ctre.hpp>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::ifstream input_file("desert_map.txt");
+    if (!input_file)
+    {
+        std::cerr << "File cannot be opened!" << std::endl;
+        return 1;
+    }
+
+    std::string instructions;
+    std::getline(input_file, instructions);
+
+    std::unordered_map<std::string, std::pair<std::string, std::string>> network;
+
+    std::string line;
+    while (std::getline(input_file, line))
+    {
+        auto matches = ctre::match<R"((\w{3}) = \((\w{3}), (\w{3})\))">(line);
+        if (matches)
+        {
+            network[matches.get<1>().to_string()] = { matches.get<2>().to_string(), matches.get<3>().to_string() };
+        }
+    }
+
+    std::string current_node = "AAA";
+    auto counter = 0;
+    size_t instruction_index = 0;
+
+    while (current_node != "ZZZ")
+    {
+        char direction = instructions[instruction_index];
+        current_node = (direction == 'L') ? network[current_node].first : network[current_node].second;
+        instruction_index = (instruction_index + 1) % instructions.size();
+        counter++;
+    }
+
+    std::cout << "Steps required to reach ZZZ: " << counter << std::endl;
+
+    return 0;
+}
---
+++ day_8/day_8_a_2.cpp
@@ -0,0 +1,58 @@
+#include <ctre.hpp>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::ifstream input_file("desert_map.txt");
+    if (!input_file)
+    {
+        std::cerr << "File cannot be opened!" << std::endl;
+        return 1;
+    }
+
+    std::string instructions;
+    std::getline(input_file, instructions);
+
+    std::unordered_map<std::string, std::pair<const std::string*, const std::string*>> network;
+    std::unordered_map<std::string, std::string> nodes;
+    std::string line;
+
+    while (std::getline(input_file, line))
+    {
+        auto matches = ctre::match<R"((\w{3}) = \((\w{3}), (\w{3})\))">(line);
+        if (matches)
+        {
+            std::string node = matches.get<1>().to_string();
+            std::string left = matches.get<2>().to_string();
+            std::string right = matches.get<3>().to_string();
+
+            nodes.insert({ node, node });
+            nodes.insert({ left, left });
+            nodes.insert({ right, right });
+
+            network[node] = { &nodes[left], &nodes[right] };
+        }
+    }
+
+    const std::string* current_node = &nodes["AAA"];
+    auto counter = 0;
+    size_t instruction_index = 0;
+    size_t instructions_size = instructions.size();
+
+    while (*current_node != "ZZZ")
+    {
+        char direction = instructions[instruction_index];
+        current_node = (direction == 'L') ? network[*current_node].first : network[*current_node].second;
+        instruction_index = (instruction_index + 1) % instructions_size;
+        counter++;
+    }
+
+    std::cout << "Steps required to reach ZZZ: " << counter << std::endl;
+
+    return 0;
+}
